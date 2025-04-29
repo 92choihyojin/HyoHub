e7a3cd6a5e760337c116d62a356e2cf52efe61e6
choi hyo jin <dkdlelrkdlrj@naver.com>
2025-04-29 17:27:25 +0900

    회사 휴가관리 프로그램을 시작

---
+++ TestHello/src/Hello/HelloMain.java
@@ -0,0 +1,11 @@
+package Hello;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요. 이클립스를 통해 처음으로 파일을 업로드합니다.");
+		System.out.println("End");
+
+	}
+
+}
